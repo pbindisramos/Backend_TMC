87420d8a39e3902a098206b8a5d2183e973c571c
pbindisramos <47041810+pbindisramos@users.noreply.github.com>
2022-04-17 15:53:24 -0400

    Hashear password

--- models/Usuario.js
+++ models/Usuario.js
@@ -1,22 +1,18 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const usuarioSchema = mongoose.Schema(
   {
-    nombre: {
-      type: String,
-      required: true,
-      trim: true
-    },
-    password: {
-      type: String,
-      required: true,
-      trim: true
-    },
     email: {
       type: String,
       required: true,
       trim: true,
       unique: true
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true
     },
     token: {
       type: String
@@ -31,6 +27,13 @@ const usuarioSchema = mongoose.Schema(
   }
 );
 
+usuarioSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 const Usuario = mongoose.model("Usuario", usuarioSchema);
 
 export default Usuario;
